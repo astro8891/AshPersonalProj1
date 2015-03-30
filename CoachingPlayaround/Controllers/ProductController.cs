7bdae51c7d5bc9a6b45171cd08f3d00aa954fbf6
Ash <mitchell.mcewen@users.noreply.github.com>
2015-03-30 20:48:31 +1300

    AshPersonalProj

---
+++ CoachingPlayaround/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoachingPlayaround.Controllers
+{
+    public class ProductController : Controller
+    {
+        public List<Item> Items = new List<Item>()
+        {
+            new Item {ID = 1, Price = 10, Name = "Bear", Description = "Salmon"},
+            new Item {ID = 2, Price = 12, Name = "Tiger", Description = "Stripes"},
+            new Item {ID = 3, Price = 14, Name = "Lion", Description = "Scary"}
+        };
+        // GET: Product
+        public ActionResult Index()
+        {
+            ViewBag.items = Items;
+            return View();
+        }
+
+        public ActionResult Details(int ID)
+        {
+            //call the Db and grab me and item with id == ID
+
+            //return that item to the view
+            Item selectedItem = null;
+
+            foreach (var item in Items)
+            {
+                if (item.ID == ID)
+                {
+                    selectedItem = item;
+                    break;
+                }
+                selectedItem = null;
+            }
+
+            ViewBag.item = selectedItem;
+
+            return View();
+        }
+
+    }
+}
---
+++ CoachingPlayaround/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoachingPlayaround
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
